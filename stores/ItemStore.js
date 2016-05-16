b07033271104ff4a8b01898fa02af9b9c88207d2
Kamil Charłampowicz <kamil.charlampowicz@netbulls.io>
2016-05-16 08:38:00 +0200

    Fetching real data from backend using fetch framework

--- stores/ItemStore.js
+++ stores/ItemStore.js
@@ -2,53 +2,56 @@
  * Item store
  * @flow
  */
-
-var MOCKED_ITEMS = [
- {
-   'id': 1,
-   'name': 'Prima',
-   'thumbnail': 'http://www.myworldhut.com/product_images/x/cafe_prima_finezja_mielona_250g__89716.jpg',
-   'score': 2.8,
-   'creator': 'Jan',
- },
- {
-   'id': 2,
-   'name': 'Lavazza - Sinfonia Espresso',
-   'thumbnail': 'http://www.kawalavazza.com.pl/images/produkty/thumbnails/lavazza_pad_espresso.jpg.thumb_96x150.jpg',
-   'score': 3.5,
-   'creator': 'Jan',
- }
-];
-
+import React, { Component } from 'react';
+var ENDPOINT_URL = 'http://192.168.10.57:8080/api/items';
 class ItemStore {
-  getItems() {
-    return MOCKED_ITEMS.sort((first, second) => second.score - first.score);
+  getItems(notify) {
+    fetch(ENDPOINT_URL)
+      .then((response) => response.json())
+      .then((json) => notify(json))
+      .catch((err) => {
+        console.log(err);
+        notify([]);
+      });
   }
 
-  addItem(name: string, thumbnail: string, creator: string) {
-    MOCKED_ITEMS.push({
-      id: MOCKED_ITEMS.length + 1,
-      name: name,
-      thumbnail: thumbnail,
-      score: 5,
-      creator: creator
+  addItem(name: string, photo: string, creator: string, notify) {
+    fetch(ENDPOINT_URL, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        'name': name,
+        'creator': creator,
+        'thumbnail': photo
+      })
+    })
+    .then((response) => notify())
+    .catch((err) => {
+      console.log(err);
+      notify();
     });
   }
 
-  markItem(item: object, rate: {name: string, score: number}) {
-    var item = MOCKED_ITEMS.find(mi => mi.id == item.id)
-    console.log(item);
-    if (item != undefined) {
-      let additionalScore = (rate.score - 5) != 0 ? (rate.score - 5) / 10 : 0;
-      let newScore = item.score + additionalScore;
-      if (newScore > 10) {
-        item.score = 10;
-      } else if (newScore < 0) {
-        item.score = 0;
-      } else {
-        item.score = Math.round(newScore * 100)/100;
-      }
-    }
+  rateItem(item: object, rate: {name: string, score: number}, notify) {
+    fetch(ENDPOINT_URL + '/' + item.id + '/review', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        'name': rate.name,
+        'score': rate.score
+      })
+    })
+    .then((response) => notify())
+    .catch((err) => {
+      console.log(err);
+      notify();
+    });
   }
 }
 
